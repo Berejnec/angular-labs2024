3d1ae37a6fc906cc6ece5bc3da42af8bf3c3b02d
aberejnec <adrian.berejnec@irian.ro>
2024-04-02 14:51:06 +0300

    Setup for l02.01.ng_pipe_final: added pizza images, use CurrencyPipe

--- PizzaApp/src/app/app.component.ts
+++ PizzaApp/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {IPizza} from "./app.model";
 import {PIZZAS} from './app.data';
+import {CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [CurrencyPipe],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
