e4b7385a33a80bc3d1c6e4151830e65fec13d225
aberejnec <adrian.berejnec@irian.ro>
2024-04-03 12:00:55 +0300

    Setup for l02.03.ng_forms_final: New review form

--- PizzaApp/src/app/tabs/tabs.component.ts
+++ PizzaApp/src/app/tabs/tabs.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
-import {IPizza} from "../app.model";
+import {IPizza, IReview} from "../app.model";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'tabs',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.css'
 })
@@ -12,8 +15,9 @@ export class TabsComponent {
   @Input() pizza!: IPizza;
 
   private activeTab = 1;
-
   tabMap = new Map<number, boolean>();
+
+  newReview: IReview = {};
 
   constructor() {
     this.tabMap.set(this.activeTab, true);
@@ -23,5 +27,11 @@ export class TabsComponent {
     this.tabMap.set(this.activeTab, false);
     this.activeTab = tid;
     this.tabMap.set(this.activeTab, true);
+  }
+
+  public onSubmit() {
+    this.newReview.createdOn = new Date().getMilliseconds();
+    this.pizza.reviews.push(this.newReview);
+    this.newReview = {};
   }
 }
