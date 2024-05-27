a5d99c16f1b471d826c688f38efd011ce391b03f
aberejnec <adrian.berejnec@irian.ro>
2024-05-27 15:34:22 +0300

    Bonus: Fix toolbar style

--- PizzaApp/src/app/app.component.ts
+++ PizzaApp/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from "@angular/router";
 import {NavigationService} from "./commons/navigation.service";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatAnchor} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatAnchor],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
