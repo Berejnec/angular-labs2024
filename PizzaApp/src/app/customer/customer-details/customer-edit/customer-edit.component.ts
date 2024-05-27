83f52fc00a02b1ae81d91f56dd3edbc313c1bf09
aberejnec <adrian.berejnec@irian.ro>
2024-05-27 15:30:55 +0300

    Setup final form

--- PizzaApp/src/app/customer/customer-details/customer-edit/customer-edit.component.ts
+++ PizzaApp/src/app/customer/customer-details/customer-edit/customer-edit.component.ts
@@ -1,24 +1,46 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {AbstractControl, AsyncValidatorFn, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators} from "@angular/forms";
 import {ICustomer} from "../../customer.model";
-import {JsonPipe} from "@angular/common";
-import {map, Observable, of} from "rxjs";
+import {AsyncPipe, JsonPipe, NgForOf, NgIf} from "@angular/common";
+import {map, Observable, of, startWith} from "rxjs";
 import {CustomerFileService} from "../../customer.service";
 import {CustomerValidators} from "../../customer-validators";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {MatButton} from "@angular/material/button";
+import {COUNTRIES} from "./countries.data";
+import {CITIES} from "./cities.data";
+import {MatAutocomplete, MatAutocompleteTrigger} from "@angular/material/autocomplete";
 
 @Component({
   selector: 'app-customer-edit',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    JsonPipe
+    JsonPipe,
+    MatFormField,
+    MatInput,
+    NgIf,
+    MatSelect,
+    MatOption,
+    NgForOf,
+    MatButton,
+    MatLabel,
+    AsyncPipe,
+    MatAutocomplete,
+    MatAutocompleteTrigger,
+    MatError
   ],
   templateUrl: './customer-edit.component.html',
   styleUrl: './customer-edit.component.css'
 })
-export class CustomerEditComponent {
+export class CustomerEditComponent implements OnInit {
   emailFormControl: FormControl;
   customerFormGroup: FormGroup;
+  filteredCities$: Observable<string[]> = of([]);
+  countries = COUNTRIES;
+  private readonly cityFormControl: FormControl;
 
   constructor(private customerService: CustomerFileService) {
     this.emailFormControl = new FormControl("",
@@ -31,6 +53,8 @@ export class CustomerEditComponent {
         this.uniqueEmailValidator()
       ]);
 
+    this.cityFormControl = new FormControl()
+
     this.customerFormGroup = new FormGroup({
       firstName: new FormControl("", [Validators.required]),
       lastName: new FormControl("", [Validators.required]),
@@ -38,7 +62,7 @@ export class CustomerEditComponent {
       phone: new FormControl(""),
       address: new FormGroup({
         street: new FormControl(),
-        city: new FormControl(),
+        city: this.cityFormControl,
         zipCode: new FormControl(),
         country: new FormControl()
       })
@@ -56,6 +80,16 @@ export class CustomerEditComponent {
     if (customer) {
       this.customerFormGroup.patchValue(customer);
     }
+  }
+
+  ngOnInit() {
+    this.filteredCities$ = this.cityFormControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const filterValue = value.toLowerCase();
+        return CITIES.filter(option => option.toLowerCase().includes(filterValue));
+      }),
+    );
   }
 
   onSubmit() {
