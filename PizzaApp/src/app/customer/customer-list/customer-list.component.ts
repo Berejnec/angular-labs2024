2d05bc5af09811e6710e8a6426221eefd2a0b719
aberejnec <adrian.berejnec@irian.ro>
2024-05-27 14:31:42 +0300

    Setup material table

--- PizzaApp/src/app/customer/customer-list/customer-list.component.ts
+++ PizzaApp/src/app/customer/customer-list/customer-list.component.ts
@@ -1,18 +1,52 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {ICustomer} from "../customer.model";
 import {NavigationService} from "../../commons/navigation.service";
 import {CustomerFileService} from "../customer.service";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable, MatTableDataSource
+} from "@angular/material/table";
+import {MatSort, MatSortHeader} from "@angular/material/sort";
+import {MatMiniFabButton} from "@angular/material/button";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-customer-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatSort,
+    MatMiniFabButton,
+    MatHeaderRow,
+    MatRow,
+    MatIcon,
+    MatRowDef,
+    MatHeaderRowDef,
+    MatPaginator,
+    MatSortHeader
+  ],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css'
 })
-export class CustomerListComponent implements OnInit {
+export class CustomerListComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ['firstName', 'lastName', 'email', 'phone', 'city', 'country', 'actions'];
+  dataSource: MatTableDataSource<ICustomer> = new MatTableDataSource<ICustomer>([]);
 
-  customers: Array<ICustomer> = [];
+  @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+  @ViewChild(MatSort) sort: MatSort | null = null;
 
   constructor(private navigationService: NavigationService,
               private customerService: CustomerFileService) {
@@ -20,7 +54,12 @@ export class CustomerListComponent implements OnInit {
 
   ngOnInit() {
     this.customerService.getCustomers()
-      .subscribe(customers => this.customers = customers);
+      .subscribe(customers => this.dataSource = new MatTableDataSource<ICustomer>(customers));
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   goToCustomer(customer: ICustomer): Promise<boolean> {
